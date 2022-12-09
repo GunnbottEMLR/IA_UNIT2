7085dc08ebdde2ae988448a5e18eae58279ebf64
GunnbottEMLR <117449087+GunnbottEMLR@users.noreply.github.com>
2022-12-09 22:39:23 +0200

    Add files via upload

---
+++ tema2.js
@@ -0,0 +1,14 @@
+
+
+
+
+function sal(){
+
+
+     
+
+        document.getElementById("second").classList.remove('nothing');
+
+         document.getElementById("first").classList.add('nothing');
+   
+        }
